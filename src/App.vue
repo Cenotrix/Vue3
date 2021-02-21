52722bfe19d521647459c37d368672a46b2bdfea
Cenotrix <matthias.matthies.business@gmx.de>
2021-02-21 23:54:31 +0100

    Heroes done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
 <template>
-    <h1>Heroes <small>{{ heroesCount }}</small></h1>
-    <ul>
-        <li v-for="(hero, index) in heroes" v-bind:key="hero.name">
-            <div>
-                {{ hero.name }}
-                <button @click="removeHero( index )" >X</button>
-            </div>
-        </li>
-    </ul>
-    <input type="text" v-model.trim="newHero">
-    <button v-on:click="addHero">Add Hero</button>
+
+    <AppHeader />
+    <AppHeroes />
+
 </template>
 
 <script>
+    import AppHeader from "./components/AppHeader"
+    import AppHeroes from "./components/AppHeroes"
+
     export default {
+        components: {
+            AppHeader,
+            AppHeroes
+        },
         data() {
-            return {
-                title: "<h1>Jetzt bin ich da</h1>",
-                newHero: 'Aquaman',
-                heroes: [
-                    { name: 'Superman' },
-                    { name: 'Batman' },
-                    { name: 'Flash' },
-                    { name: 'Arrow' },
-                ],
-                isShowing: true
-            }
+            return {}
         },
-        methods: {
-            addHero: function() { 
-                if( this.newHero != '' ) {
-                    this.heroes.push({name: this.newHero})
-                    this.newHero = ''
-                }
-            },
-            removeHero: function( index ) {
-                this.heroes = this.heroes.filter( ( hero, i ) => i != index )
-            }
-        },
-        computed: {
-            heroesCount() {
-                return this.heroes.length;
-            }
-        }
+        methods: {},
+        computed: {}
     }
 </script>
---
+++ src/components/AppHeroes.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Heroes <small>{{ heroesCount }}</small></h1>
+    <ul>
+        <li v-for="(hero, index) in heroes" v-bind:key="hero.name">
+            <div>
+                {{ hero.name }}
+                <button @click="removeHero( index )" >X</button>
+            </div>
+        </li>
+    </ul>
+    <input type="text" v-model.trim="newHero">
+    <button v-on:click="addHero">Add Hero</button>
+</template>
+
+<script>
+export default {
+        data() {
+            return {
+                title: "<h1>Jetzt bin ich da</h1>",
+                newHero: 'Aquaman',
+                heroes: [
+                    { name: 'Superman' },
+                    { name: 'Batman' },
+                    { name: 'Flash' },
+                    { name: 'Arrow' },
+                ],
+                isShowing: true
+            }
+        },
+        methods: {
+            addHero: function() { 
+                if( this.newHero != '' ) {
+                    this.heroes.push({name: this.newHero})
+                    this.newHero = ''
+                }
+            },
+            removeHero: function( index ) {
+                this.heroes = this.heroes.filter( ( hero, i ) => i != index )
+            }
+        },
+        computed: {
+            heroesCount() {
+                return this.heroes.length;
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
